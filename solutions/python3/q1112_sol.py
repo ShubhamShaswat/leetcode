66ffa1b541f3e8306f0e4c93bdb09a222ad9a877
ShubhamShaswat <chandragupta4115@gmail.com>
2020-01-15 16:55:23 +0530

    modified q1112_sol.py with the requested changes

--- solutions/python3/q1112_sol.py
+++ solutions/python3/q1112_sol.py
@@ -1,33 +1,32 @@
-#created a base dictionary set to count frequency of the letter in the words
-base_letters = {chr(i):0 for i in range(ord('a'),ord('z')+1)}
-                                        
+class Solution:
 
-def wordsFormed(words,chars):
+    #this class consist the solutions for Q-1112
 
-#intillizing result
-                                        result = 0
-                                        
-#count the frequency of each charaters in chars
-                                        for c in list(chars):
-                                            base_letters[c] +=1 #
-                                        
-                                        for word in words:
-                                            cnt = 0
-                                            new_base_letters = base_letters.copy() #copy the dictionary
-                                            print(new_base_letters)
-                                            for ch in list(word):
-                                                if new_base_letters[ch] >= 1:
-                                                    new_base_letters[ch] -= 1 #derement the character count
-                                                    cnt +=1
-                                                else:
-                                                    break #break out of the loop 
-                                            print(cnt)
-                                            if cnt == len(word):
-                                                result += len(word)
-                                        
-                                        return result
-                                            
-                                        
-                                        
-                                        
-                                        
+    def __init__(self):
+        #created a base dictionary set to count frequency of the letter in the words
+        self.base_letters = {chr(i):0 for i in range(ord('a'),ord('z')+1)}
+
+
+    def countCharacters(self,words,chars):
+
+        #intillizing result
+        result = 0
+
+        #count the frequency of each charaters in chars
+        for c in list(chars):
+            self.base_letters[c] +=1
+
+        for word in words:
+            cnt = 0
+            new_base_letters = self.base_letters.copy() #copy the dictionary
+            for ch in list(word):
+                if new_base_letters[ch] >= 1:
+                    new_base_letters[ch] -= 1 #decrement the character count
+                    cnt +=1
+                else:
+                    break #break out of the loop
+
+            if cnt == len(word):
+                result += len(word)
+
+        return result
